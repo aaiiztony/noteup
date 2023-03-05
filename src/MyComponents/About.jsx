e1f7d445e708816dbbaff3f5bc81c6e215c9c273
Aaiiztony <aaiiztony@gmail.com>
2023-03-05 23:06:22 +0530

    useContext + useState

--- src/MyComponents/About.jsx
+++ src/MyComponents/About.jsx
@@ -1,11 +1,15 @@
-import React, { useContext } from 'react'
-import noteContext from '../context/notes/NoteContext'
+import React, { useContext, useEffect } from 'react'
+import NoteContext from '../context/notes/NoteContext'
 
 const About = () => {
-    const a = useContext(noteContext)
+    const a = useContext(NoteContext)
+    useEffect(()=>{
+      a.updatedState()
+      // eslint-disable-next-line 
+    }, [])
   return (
     <div>
-      <h2>Hi, I'm {a.name} and I'm a {a.job}</h2>
+      <h2>Hi, I'm {a.state.name} and I'm a {a.state.job}</h2>
     </div>
   )
 }
--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import NoteContext from "./NoteContext";
 const NoteState = (props) => {
-    let state = {
+    let var1 = {
         name:"Tony",
         job:"Web Developer"
     }
+    const [state, setState] = useState(var1)
+    const updatedState = ()=>{
+      setTimeout(() => {
+        setState({
+          name:"Prasenjeet",
+          job:"Dark Empath"
+        })
+      }, 1000);
+    }
   return (
-    <NoteContext.Provider value={state}>
+    <NoteContext.Provider value={{state, updatedState}}>
     {props.children}
     </NoteContext.Provider>
     )
